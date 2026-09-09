a909ee01fe71c1be2e4080033a5885193519d803
Nandita <nandita.tester@gmail.com>
2026-09-09 12:18:47 +0530

    Implementing Queue as LinkedList

---
+++ queue_linkedlist.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+struct QueueNode{
+    int val;
+    QueueNode* next;
+};
+
+QueueNode* front;
+QueueNode* rear;
+
+void Insert(QueueNode* &front, QueueNode* &rear, int value)
+{
+    QueueNode* temp = new QueueNode();
+    temp->val = value;
+    temp->next = NULL;
+
+    if (front==NULL)
+    {
+        front = temp;
+        rear = temp;
+    }
+
+    else
+    {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+int Delete(QueueNode* &front, QueueNode* &rear)
+{
+    QueueNode* current = front;
+    if(front==NULL)
+    {
+        cout<<"Queue is Empty \n";
+        return -1;
+    }
+    int element = current->val;
+    if (front==rear)
+    {
+        front = rear = NULL;
+    }
+    else
+    {
+        front = current->next;
+        free(current);
+    }
+    return element;
+}
+
+void Print(QueueNode* front)
+{
+    QueueNode* current = front;
+    while(current!=NULL)
+    {
+        cout<<current->val<<" ";
+        current = current->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    front = NULL;
+    rear = NULL;
+    int x=1, choice, value, del;
+    while (x==1)
+    {
+        cout<<"Enter: \n1. Insert \n2. Delete \n3. Print \n0. Exit \n";
+        cin>>choice;
+        switch (choice)
+        {
+            case 1:
+            cout<<"Enter value to be inserted: ";
+            cin>>value;
+            Insert(front, rear, value);
+            Print(front);
+            break;
+
+            case 2:
+            del = Delete(front, rear);
+            if (del==-1)
+            break;
+
+            cout<<"Deleted Element: "<<del<<endl;
+            Print(front);
+            break;
+
+            case 3:
+            cout<<"Printing Queue: \n";
+            Print(front);
+            break;
+
+            case 0:
+            cout<<"Exiting...\n";
+            x=0;
+            break;
+
+            default:
+            cout<<"Invalid Input \n";
+        }
+    }
+    return 0;
+}
+//run using: 
+//g++ queue_linkedlist.cpp -o queue_linkedlist
+//./queue_linkedlist
